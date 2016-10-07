d0c85d0db9faa8ee2c5269da7778e52660cf68a5
assistiverobotics <andlydakis@gmail.com>
2016-10-07 17:33:20 -0400

    scripts now copy the exercises correctly

--- launch/copy_one.sh
+++ launch/copy_one.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp  ~/ros_ws/src/myo_raw/myo_mdp/data/* ~/ros_ws/src/myo_raw/myo_mdp/data1/
-#touch ~/ros_ws/src/myo_raw/myo_mdp/data1/mdp.pkl
+touch ~/ros_ws/src/myo_raw/myo_mdp/data1/mdp.pkl
+date >> ~/ros_ws/src/myo_raw/myo_mdp/data1/mdp.pkl
--- launch/copy_three.sh
+++ launch/copy_three.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp  ~/ros_ws/src/myo_raw/myo_mdp/data/* ~/ros_ws/src/myo_raw/myo_mdp/data3/
-#touch ~/ros_ws/src/myo_raw/myo_mdp/data3/mdp.pkl
+touch ~/ros_ws/src/myo_raw/myo_mdp/data3/mdp.pkl
+date >> ~/ros_ws/src/myo_raw/myo_mdp/data3/mdp.pkl
